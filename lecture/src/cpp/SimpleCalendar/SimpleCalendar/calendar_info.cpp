de0cbd12a0ef50ad6d375b69ad490687ca9a6c95
mac-novice-pg2 <mac-novice-pg2@users.noreply.github.com>
2019-01-06 01:39:15 +0900

    処理の内容は変えず、文章のみ変更

--- lecture/src/cpp/SimpleCalendar/SimpleCalendar/calendar_info.cpp
+++ lecture/src/cpp/SimpleCalendar/SimpleCalendar/calendar_info.cpp
@@ -4,29 +4,32 @@
 
 MonthInfo
 month_info_2019[] = {
-    { 31, eTue }, // 2019/01
-    { 28, eFri }, // 2019/02
-    { 31, eFri }, // 2019/03
-    { 30, eMon }, // 2019/04
-    { 31, eWed }, // 2019/05
-    { 30, eSat }, // 2019/06
-    { 31, eMon }, // 2019/07
-    { 31, eThu }, // 2019/08
-    { 30, eSun }, // 2019/09
-    { 31, eTue }, // 2019/10
-    { 30, eFri }, // 2019/11
-    { 31, eSun }, // 2019/12
+    // eom, start_weekday
+    {  31,  eTue }, // 2019/01
+    {  28,  eFri }, // 2019/02
+    {  31,  eFri }, // 2019/03
+    {  30,  eMon }, // 2019/04
+    {  31,  eWed }, // 2019/05
+    {  30,  eSat }, // 2019/06
+    {  31,  eMon }, // 2019/07
+    {  31,  eThu }, // 2019/08
+    {  30,  eSun }, // 2019/09
+    {  31,  eTue }, // 2019/10
+    {  30,  eFri }, // 2019/11
+    {  31,  eSun }, // 2019/12
 };
 
 EventInfo
 event_info_2019[][ EVENT_ITEM_MAX ] = {
     { // 2019/1
+        // { day, event_name, is_holiday}
         { 1, "元旦", true },
         { 7, "七草の日", false },
         { 14,"成人の日", true },
         { EVENT_END,nullptr, false },
     },
     { // 2019/2
+        // { day, event_name, is_holiday}
         { 3, "節分", false },
         { 4, "立春", false },
         { 11, "建国記念日", true },
@@ -34,6 +37,7 @@ event_info_2019[][ EVENT_ITEM_MAX ] = {
         { EVENT_END,nullptr, false },
     },
     { // 2019/3
+        // { day, event_name, is_holiday}
         { 3, "ひな祭り", false },
         { 4, "立春", false },
         { 14, "ホワイトデー", false },
@@ -41,11 +45,13 @@ event_info_2019[][ EVENT_ITEM_MAX ] = {
         { EVENT_END,nullptr, false },
     },
     { // 2019/4
+        // { day, event_name, is_holiday}
         { 1, "エイプリルフール", false },
         { 29, "昭和の日", true },
         { EVENT_END,nullptr },
     },
     { // 2019/5
+        // { day, event_name, is_holiday}
         { 1, "メーデー", false },
         { 3, "憲法記念日", true },
         { 4, "みどりの日", true },
@@ -54,10 +60,12 @@ event_info_2019[][ EVENT_ITEM_MAX ] = {
         { EVENT_END,nullptr, false },
     },
     { // 2019/6
+        // { day, event_name, is_holiday}
         { 1, "衣替え", false },
         { EVENT_END,nullptr, false },
     },
     { // 2019/7
+        // { day, event_name, is_holiday}
         { 7, "七夕", false },
         { 15, "海の日", true },
         { 17, "祇園祭", false },
@@ -65,24 +73,28 @@ event_info_2019[][ EVENT_ITEM_MAX ] = {
         { EVENT_END,nullptr, false },
     },
     { // 2019/8
+        // { day, event_name, is_holiday}
         { 11, "山の日", true },
         { 12, "振替休日", true },
         { 15, "終戦記念日", true },
         { EVENT_END,nullptr, false },
     },
     { // 2019/9
+        // { day, event_name, is_holiday}
         { 15, "月見", false },
         { 16, "敬老の日", true },
         { 23, "秋分の日", true },
         { EVENT_END,nullptr, false },
     },
     { // 2019/10
+        // { day, event_name, is_holiday}
         { 1, "衣替え", false },
         { 14, "体育の日", true },
         { 23, "秋分の日", true },
         { EVENT_END,nullptr, false },
     },
     { // 2019/12
+        // { day, event_name, is_holiday}
         { 23, "天皇誕生日？", true },
         { 24, "クリスマスイブ", false },
         { 25, "クリスマス", false },
