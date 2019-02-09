1786bae62f6dd18b88adb2bd7458f0f1af4c0bec
mac-novice-pg2 <mac_iwa4ta@yahoo.co.jp>
2019-02-09 18:49:53 +0900

    プリンタクラスのコード修正

--- lecture/src/cpp/SimpleCalendar/SimpleCalendar/CalendarPrinter.cpp
+++ lecture/src/cpp/SimpleCalendar/SimpleCalendar/CalendarPrinter.cpp
@@ -1,18 +1,17 @@
 #include "pch.h"
 #include "CalendarPrinter.h"
 
-
 CalendarPrinter::CalendarPrinter()
 {
     m_output = stdout;
     m_output_mode = eOutputMode::eMonitor;
-}
+} // CalendarPrinter::CalendarPrinter()
 
 CalendarPrinter::CalendarPrinter( const char *filename )
 {
     m_output = fopen( filename, "wt" );
     m_output_mode = eOutputMode::eFile;
-}
+} // CalendarPrinter::CalendarPrinter()
 
 CalendarPrinter::~CalendarPrinter()
 {
@@ -20,10 +19,10 @@ CalendarPrinter::~CalendarPrinter()
     {
         fclose( m_output );
     }
-}
+} // CalendarPrinter::~CalendarPrinter()
 
 FILE*
-CalendarPrinter::Print( void )
+CalendarPrinter::Output( void )
 {
     return m_output;
-}
+} // CalendarPrinter::Output()
