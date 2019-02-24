24c037c9459a5ee7e910855c6f32376f680cc28f
mac-novice-pg2 <mac_iwa4ta@yahoo.co.jp>
2019-02-24 10:02:57 +0900

    月齢/干支関数を追加

--- lecture/src/cpp/SimpleCalendarDev/SimpleCalendar/CMonthInfo.h
+++ lecture/src/cpp/SimpleCalendarDev/SimpleCalendar/CMonthInfo.h
@@ -4,6 +4,8 @@
 class CMonthInfo
 {
 public:
+    double Calc_MoonAge( int year, int month, int day );
+    const char* Calc_ETO( int year );
     static bool Is_LeapYear( int year );
     static eWeekday Formula_Zeller( int year, int month, int day );
     static int GetEndOfMonth( int year, int month );
