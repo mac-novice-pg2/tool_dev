75c83b1872447de7fcbd05c41399f8a16509326c
mac-novice-pg2 <mac_iwa4ta@yahoo.co.jp>
2019-03-09 18:01:16 +0900

    祝日情報を追加

--- lecture/src/cs/EventManager/EventManager/Form1.Designer.cs
+++ lecture/src/cs/EventManager/EventManager/Form1.Designer.cs
@@ -35,6 +35,10 @@
             this.イベント内容テキスト = new System.Windows.Forms.TextBox();
             this.イベント日ラベル = new System.Windows.Forms.Label();
             this.イベント内容ラベル = new System.Windows.Forms.Label();
+            this.休日グループ = new System.Windows.Forms.GroupBox();
+            this.休日チェック = new System.Windows.Forms.RadioButton();
+            this.平日チェック = new System.Windows.Forms.RadioButton();
+            this.休日グループ.SuspendLayout();
             this.SuspendLayout();
             // 
             // イベントカレンダー
@@ -49,12 +53,12 @@
             this.イベント表.Location = new System.Drawing.Point(223, 12);
             this.イベント表.Multiline = true;
             this.イベント表.Name = "イベント表";
-            this.イベント表.Size = new System.Drawing.Size(260, 262);
+            this.イベント表.Size = new System.Drawing.Size(260, 301);
             this.イベント表.TabIndex = 1;
             // 
             // 登録ボタン
             // 
-            this.登録ボタン.Location = new System.Drawing.Point(18, 242);
+            this.登録ボタン.Location = new System.Drawing.Point(18, 279);
             this.登録ボタン.Name = "登録ボタン";
             this.登録ボタン.Size = new System.Drawing.Size(75, 23);
             this.登録ボタン.TabIndex = 2;
@@ -94,11 +98,45 @@
             this.イベント内容ラベル.TabIndex = 6;
             this.イベント内容ラベル.Text = "イベント内容";
             // 
+            // 休日グループ
+            // 
+            this.休日グループ.Controls.Add(this.平日チェック);
+            this.休日グループ.Controls.Add(this.休日チェック);
+            this.休日グループ.Location = new System.Drawing.Point(18, 233);
+            this.休日グループ.Name = "休日グループ";
+            this.休日グループ.Size = new System.Drawing.Size(199, 40);
+            this.休日グループ.TabIndex = 7;
+            this.休日グループ.TabStop = false;
+            this.休日グループ.Text = "休日/祝日";
+            // 
+            // 休日チェック
+            // 
+            this.休日チェック.AutoSize = true;
+            this.休日チェック.Location = new System.Drawing.Point(6, 18);
+            this.休日チェック.Name = "休日チェック";
+            this.休日チェック.Size = new System.Drawing.Size(47, 16);
+            this.休日チェック.TabIndex = 8;
+            this.休日チェック.TabStop = true;
+            this.休日チェック.Text = "休日";
+            this.休日チェック.UseVisualStyleBackColor = true;
+            // 
+            // 平日チェック
+            // 
+            this.平日チェック.AutoSize = true;
+            this.平日チェック.Location = new System.Drawing.Point(67, 18);
+            this.平日チェック.Name = "平日チェック";
+            this.平日チェック.Size = new System.Drawing.Size(47, 16);
+            this.平日チェック.TabIndex = 9;
+            this.平日チェック.TabStop = true;
+            this.平日チェック.Text = "平日";
+            this.平日チェック.UseVisualStyleBackColor = true;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(493, 279);
+            this.ClientSize = new System.Drawing.Size(493, 325);
+            this.Controls.Add(this.休日グループ);
             this.Controls.Add(this.イベント内容ラベル);
             this.Controls.Add(this.イベント日ラベル);
             this.Controls.Add(this.イベント内容テキスト);
@@ -108,6 +146,8 @@
             this.Controls.Add(this.イベントカレンダー);
             this.Name = "Form1";
             this.Text = "イベントマネージャー";
+            this.休日グループ.ResumeLayout(false);
+            this.休日グループ.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -122,6 +162,9 @@
         private System.Windows.Forms.TextBox イベント内容テキスト;
         private System.Windows.Forms.Label イベント日ラベル;
         private System.Windows.Forms.Label イベント内容ラベル;
+        private System.Windows.Forms.GroupBox 休日グループ;
+        private System.Windows.Forms.RadioButton 平日チェック;
+        private System.Windows.Forms.RadioButton 休日チェック;
     }
 }
 
