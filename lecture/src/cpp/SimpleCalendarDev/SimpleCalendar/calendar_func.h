be3326cb8a143bce6dde343b9298da2e5b8b47db
mac-novice-pg2 <mac_iwa4ta@yahoo.co.jp>
2019-03-24 01:36:31 +0900

    プロジェクト整理

--- lecture/src/cpp/SimpleCalendarDev/SimpleCalendar/calendar_func.h
+++
@@ -1,8 +0,0 @@
-﻿#pragma once
-
-#include "CEventManager.h"
-
-// APIプロトタイプ宣言
-void PrintToday( void );
-void PrintCalendar( int year, int month );
-void PrintEventAlert( int year, int month, CEventManager *holiday );
--- lecture/src/cpp/SimpleCalendarDev/SimpleCalendar/common_func.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-
-#include <vector>
-
-using string_container = std::vector< std::string >;
-
-string_container
-Split( const std::string& work, char splitter );
