5560e55d95cc6ca12f521512fef753474126e99e
mac-novice-pg2 <mac_iwa4ta@yahoo.co.jp>
2019-05-04 14:43:30 +0900

    プロジェクト名を改めて新規作成＆画面遷移追加

---
+++ lecture/src/cpp/CookingPlanner/include/Recipe.h
@@ -0,0 +1,83 @@
+﻿#pragma once
+
+#include <vector>
+
+#include "SearchType.h"
+
+struct SearchCondition{
+    int item_no;
+    char name[ 32 ];
+    int time;
+    int cost;
+    int kcal;
+    eFoodGenre genre;
+    eSeason season;
+    eDifficulty difficulty;
+    eFoodStyle type;
+
+    SearchCondition()
+    {
+        SetPreset_Init();
+    }
+
+    void SetPreset_Init();
+    void SetPreset_Diet();
+    void SetPreset_CostSaving();
+
+    bool IsMatch_name( const char *check );
+    bool IsMatch_time( int check );
+    bool IsMatch_cost( int check );
+    bool IsMatch_kcal( int check );
+    bool IsMatch_genre( eFoodGenre check );
+    bool IsMatch_season( eSeason check );
+    bool IsMatch_difficulty( eDifficulty check );
+    bool IsMatch_type( eFoodStyle check );
+};
+
+struct SearchInfo{
+    int item_no;
+    char name[ 32 ];
+    int time;
+    int cost;
+    int kcal;
+    char kitchenware[ 32 ];
+
+    eFoodGenre genre;
+    eSeason season;
+    eDifficulty difficulty;
+    eFoodStyle type;
+
+    char recipe[ 1024 ];
+    char ingredients[ 256 ];
+
+    SearchInfo()
+        : item_no( 0 ), name( "" ), time( 0 ), cost( 0 ), kcal( 0 ),
+        genre( eFoodGenre::Japanese_Food ),
+        season( eSeason::AllSeason ),
+        difficulty( eDifficulty::Easy_Difficulty ),
+        type( eFoodStyle::AllSituation )
+    {}
+
+    const char *ToString();
+};
+
+using SearchInfoList = std::vector< SearchInfo >;
+
+class Recipe{
+    SearchInfoList search_info_;
+    SearchInfoList search_result_;
+
+    void set_numeric_field( FILE *fp, int *out );
+    const char* read_string_param( FILE *fp );
+
+    void create_search_list( const char *recipe_fname );
+    SearchInfo read_entry( FILE *fp );
+    Recipe();
+
+public:
+    Recipe( const char *recipe_fname );
+    virtual ~Recipe();
+
+    void ShowDishList();
+    const SearchInfoList& Search( SearchCondition *cond );
+};
---
+++ lecture/src/cpp/CookingPlanner/include/SearchType.h
@@ -0,0 +1,40 @@
+﻿#pragma once
+
+enum eFoodGenre
+{
+    Japanese_Food,
+    Western_Food,
+    Chinese_Food,
+    Italian_Food,
+    French_Food,
+    Stateless_Food,
+    Invalid_Food,
+};
+
+enum eSeason{
+    Spring,
+    Summer,
+    Autumn,
+    Winter,
+    AllSeason,
+    Invalid_Season,
+};
+
+enum eDifficulty{
+    Hard_Difficulty,
+    Normal_Difficulty,
+    Easy_Difficulty,
+    Invalid_Difficulty,
+};
+
+enum eFoodStyle{
+    Breakfast,
+    Lunch,
+    Dinner,
+    Desert,
+    LightMeal,
+    HomeParty,
+    Outdoor,
+    AllSituation,
+    Invalid_FoodStyle,
+};
